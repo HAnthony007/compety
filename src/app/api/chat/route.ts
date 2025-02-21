dd800958149fbfbcbfb9485160f799b7d0ad875b
HAnthony007 <harifetraanthonyr@gmail.com>
2025-02-21 22:23:19 +0300

    update system prompt for markdown style&emoji

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,6 +1,6 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
-import { streamText, tool } from "ai";
-import { z } from "zod";
+import { streamText } from "ai";
+import { systemPrompt } from "./prompt";
 
 export const maxDuration = 30;
 
@@ -11,27 +11,9 @@ const google = createGoogleGenerativeAI({
 export async function POST(req: Request) {
     const { messages } = await req.json();
     const result = streamText({
+        system: systemPrompt,
         model: google("gemini-1.5-flash"),
         messages,
-        tools: {
-            weather: tool({
-                description: "Get the weather in a location (fahreinheit)",
-                parameters: z.object({
-                    location: z
-                        .string()
-                        .describe("The location to get the weather for"),
-                }),
-                execute: async ({ location }) => {
-                    const temperature = Math.round(
-                        Math.random() * (90 - 72) + 32
-                    );
-                    return {
-                        location,
-                        temperature,
-                    };
-                },
-            }),
-        },
     });
     return result.toDataStreamResponse();
 }
