884623fd967241e8adf15f440367b2b9a22cd5f1
Giovanni <giovanniandriamihaja9@gmail.com>
2025-02-28 23:52:15 +0300

    Ajout FaceId

---
+++ src/app/reco/page.tsx
@@ -0,0 +1,9 @@
+import FaceRecognition from "@/components/FaceRecognition";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <FaceRecognition />
+    </main>
+  );
+}
